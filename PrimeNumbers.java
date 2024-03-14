bd89354869f1122363d9419e5e30c18067f99cbe
Akshata <akshatabhopate712@gmail.com>
2024-03-14 13:26:34 +0530

    Prime Numbers

---
+++ PrimeNumbers.java
@@ -0,0 +1,24 @@
+package forLoopExamples;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+//Prime Number
+	public static void main(String[] args) {
+		int i;
+		int j;
+		for (i = 1; i <= 10; i++) {
+			int count = 0;
+			for (j = 1; j <= i; j++) {
+				if (i % j == 0) {
+					count++;
+				}
+			}
+			if (count == 2) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
