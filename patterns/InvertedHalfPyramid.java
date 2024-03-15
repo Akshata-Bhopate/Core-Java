49998786421c02ed4bbf4137783c08eaf4b388a9
Akshata <akshatabhopate712@gmail.com>
2024-03-15 20:49:06 +0530

    Patterns Examples

---
+++ patterns/InvertedHalfPyramid.java
@@ -0,0 +1,23 @@
+package patterns;
+/*   *****
+ *   ****
+ *   ***
+ *   **
+ *   *
+ */
+
+public class InvertedHalfPyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		for (int i=a; i>=1; i--) {
+			for (int j = 1; j <=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
